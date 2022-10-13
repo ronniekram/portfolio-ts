bbb47e072b8b207ecf8aa74e2606b38e227fc765
ronniekram <ronnie@actualsize.com>
2022-10-12 22:00:07 -0400

    working: tailwind config

---
+++ web/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  theme: {
+    color: {
+      purple: {
+        light: `#B573BE`,
+        mid: `#7C3091`,
+        dark: `#3F0150`,
+      },
+      orange: {
+        light: `#FFF5E9`,
+        mid: `#FC7B53`,
+        dark: `#DC2400`,
+      },
+      pink: `#EFBFE9`,
+      black: `#000000`,
+      white: `#FFFFFF`,
+      transparent: `transparent`,
+      current: `currentColor`,
+    },
+    fontFamily: {
+      hack: `Hack`
+    }
+  },
+  plugins: [],
+};
